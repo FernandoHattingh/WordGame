9f0952151ba6210e9f2b9702bbd790dcaaa91578
DISD3 <DISD3@ML-RefVm-631348>
2023-08-18 09:23:30 +0200

    Add project files.

---
+++ WordGame/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WordGame
+{
+    public partial class MainWindow : Window
+    {
+        List<string> wordList = new List<string>();
+        Random wordSelector = new Random();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        public void btnPlay_Click(object sender, RoutedEventArgs e)
+        {           
+            wordList.Add("Processor");
+            wordList.Add("Compiler");
+            wordList.Add("Array");
+            wordList.Add("Counter");
+            
+
+            lblScrambledWord.Content = (ScrambleWord(wordList[SelectWord()]).ToString());
+
+        }
+
+        public int SelectWord()
+        {
+            int word = wordSelector.Next(wordList.Count);
+
+            return word;
+        }
+
+        public string ScrambleWord(string word)
+        {
+            char[] chars = new char[word.Length];
+            Random rando = new Random(100);
+            int index = 0;
+            while (word.Length > 0)
+            {
+                int next = rando.Next(0, word.Length - 1);
+                                                        
+                chars[index] = word[next];                
+                word = word.Substring(0, next) + word.Substring(next + 1);
+                ++index;
+            }
+            return new String(chars);
+        }
+
+        public string CheckMatch(string match) 
+        {
+            string selectedWord = wordList[SelectWord()];
+
+            if (match.Equals(selectedWord))
+            {
+                match = "Correct";
+            }
+            else
+            {
+                match = "Incorrect"; ;
+            }
+
+            return match;
+        }
+
+
+        public void btnCheck_Click(object sender, RoutedEventArgs e)
+        {
+           string userIn = txtbAnswer.Text;
+            MessageBox.Show(CheckMatch(userIn));
+           
+
+        }
+    }
+}
